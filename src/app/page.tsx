ca5a47969645e61286ee9b73d81cb1b5157f52c1
Rigil Kent Y. Payo <rigilkentpayo@gmail.com>
2025-11-12 03:32:50 +0800

    Redirect / to /login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,2 +1,6 @@
+// src/app/page.tsx
 import { redirect } from "next/navigation";
-export default function Home() { redirect("/login"); }
+
+export default function Home() {
+    redirect("/login");
+}

f8646e7611c11e9ce172418f0b3563e7077e8817
Rigil Kent Y. Payo <rigilkentpayo@gmail.com>
2025-11-12 23:13:38 +0800

    fix: added build:domain, deploy scripts and next.config updates

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 
 const deploy = process.env.DEPLOY_TARGET ?? process.env.NEXT_PUBLIC_TARGET ?? "";
 const isProd = process.env.NODE_ENV === "production";
-// Only treat as gh-pages when building for production
 const isGhPages = isProd && deploy === "ghpages";
 const repo = "/solmate-admin";
 
@@ -11,10 +10,9 @@ const nextConfig: NextConfig = {
     output: "export",
     images: { unoptimized: true },
     trailingSlash: true,
-    // In dev: no basePath/assetPrefix -> avoids 404 for HMR/chunks
     basePath: isGhPages ? repo : "",
     assetPrefix: isGhPages ? `${repo}/` : "",
-    eslint: { ignoreDuringBuilds: true },
+    eslint: { ignoreDuringBuilds: true }
 };
 
 export default nextConfig;
